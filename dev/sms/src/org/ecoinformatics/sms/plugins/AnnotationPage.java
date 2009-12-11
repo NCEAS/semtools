8099d43b84c3c6656445ade989c35f3eb0b0e7d6
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2009-12-11 19:36:30 +0000

    source->format

--- dev/sms/src/org/ecoinformatics/sms/plugins/AnnotationPage.java
+++ dev/sms/src/org/ecoinformatics/sms/plugins/AnnotationPage.java
@@ -56,163 +56,168 @@ import edu.ucsb.nceas.morpho.plugins.datapackagewizard.WizardSettings;
 import edu.ucsb.nceas.morpho.util.UISettings;
 import edu.ucsb.nceas.utilities.OrderedMap;
 
+public class AnnotationPage extends AbstractUIPage {
 
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// *
 
-public class AnnotationPage extends AbstractUIPage{
+	private final String pageID = DataPackageWizardInterface.USAGE_RIGHTS;
+	private final String pageNumber = "9";
+	private final String title = "Attribute Annotation Editor";
+	private final String subtitle = "";
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// *
 
-  private final String pageID     = DataPackageWizardInterface.USAGE_RIGHTS;
-  private final String pageNumber = "9";
-  private final String title      = "Attribute Annotation Editor";
-  private final String subtitle   = "";
+	private JTextField observationEntity;
+	private JTextField observationCharacteristic;
+	private JTextField observationStandard;
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	private Annotation annotation = null;
+	private Observation currentObservation;
+	private Mapping currentMapping;
+	private Measurement currentMeasurement;
+	private Characteristic currentCharacteristic;
+	private Standard currentStandard;
 
-  private JTextField observationEntity;
-  private JTextField observationCharacteristic;
-  private JTextField observationStandard;
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// *
 
-  private Annotation annotation = null;
-private Observation currentObservation;
-private Mapping currentMapping;
-private Measurement currentMeasurement;
-private Characteristic currentCharacteristic;
-private Standard currentStandard;
-    
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	public AnnotationPage() {
+		nextPageID = DataPackageWizardInterface.GEOGRAPHIC;
+		init();
+	}
 
-  public AnnotationPage() 
-  {
-	  nextPageID = DataPackageWizardInterface.GEOGRAPHIC;
-	  init(); 
-  }
+	/**
+	 * initialize method does frame-specific design - i.e. adding the widgets
+	 * that are displayed only in this frame (doesn't include prev/next buttons
+	 * etc)
+	 */
+	private void init() {
 
-  /**
-   * initialize method does frame-specific design - i.e. adding the widgets that
-   * are displayed only in this frame (doesn't include prev/next buttons etc)
-   */
-  private void init() {
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-    this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JLabel desc = WidgetFactory
+				.makeHTMLLabel(
+						"<b>Select Observation Entity, Characteristic and Standard for the selected attribute.</b> "
+								+ "The Ontology Browser can be used to navigate specific ontologies.",
+						2);
+		this.add(desc);
 
-    JLabel desc = WidgetFactory.makeHTMLLabel(
-      "<b>Select Observation Entity, Characteristic and Standard for the selected attribute.</b> "
-      +"The Ontology Browser can be used to navigate specific ontologies.", 2);
-    this.add(desc);
+		this.add(WidgetFactory.makeDefaultSpacer());
 
-    this.add(WidgetFactory.makeDefaultSpacer());
-
-    MouseListener mListener = 
-    	new MouseAdapter() {
+		MouseListener mListener = new MouseAdapter() {
 
 			public void mouseClicked(MouseEvent e) {
 				JTextField source = (JTextField) e.getSource();
 				showDialog(source);
 			}
-    	
-    };
-    JPanel classesPanel = WidgetFactory.makePanel();
-    classesPanel.setLayout(new BoxLayout(classesPanel, BoxLayout.Y_AXIS));
-    
-    // Entity
-    JPanel entityPanel = WidgetFactory.makePanel(1);
-    entityPanel.add(WidgetFactory.makeLabel("Entity:", false));
-    observationEntity = WidgetFactory.makeOneLineTextField("<entity>");
-    observationEntity.addMouseListener(mListener);
-    entityPanel.add(observationEntity);
-    entityPanel.setBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 8 * WizardSettings.PADDING));
-    
-    // Characteristic
-    JPanel characteristicPanel = WidgetFactory.makePanel(1);
-    characteristicPanel.add(WidgetFactory.makeLabel("Characteristic:", false));
-    observationCharacteristic = WidgetFactory.makeOneLineTextField("<characteristic>");
-    observationCharacteristic.addMouseListener(mListener);
 
-    characteristicPanel.add(observationCharacteristic);
-    characteristicPanel.setBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 8 * WizardSettings.PADDING));
-    
-    // Standard
-    JPanel standardPanel = WidgetFactory.makePanel(1);
-    standardPanel.add(WidgetFactory.makeLabel("Standard:", false));
-    observationStandard = WidgetFactory.makeOneLineTextField("<standard>");
-    observationStandard.addMouseListener(mListener);
+		};
+		JPanel classesPanel = WidgetFactory.makePanel();
+		classesPanel.setLayout(new BoxLayout(classesPanel, BoxLayout.Y_AXIS));
 
-    standardPanel.add(observationStandard);
-    standardPanel.setBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 8 * WizardSettings.PADDING));
-    
-    //put them together
-    classesPanel.add(entityPanel);
-    classesPanel.add(WidgetFactory.makeDefaultSpacer());
-    classesPanel.add(characteristicPanel);
-    classesPanel.add(WidgetFactory.makeDefaultSpacer());
-    classesPanel.add(standardPanel);
+		// Entity
+		JPanel entityPanel = WidgetFactory.makePanel(1);
+		entityPanel.add(WidgetFactory.makeLabel("Entity:", false));
+		observationEntity = WidgetFactory.makeOneLineTextField("<entity>");
+		observationEntity.addMouseListener(mListener);
+		entityPanel.add(observationEntity);
+		entityPanel.setBorder(new javax.swing.border.EmptyBorder(0, 0, 0,
+				8 * WizardSettings.PADDING));
 
-    this.add(classesPanel);
+		// Characteristic
+		JPanel characteristicPanel = WidgetFactory.makePanel(1);
+		characteristicPanel.add(WidgetFactory.makeLabel("Characteristic:",
+				false));
+		observationCharacteristic = WidgetFactory
+				.makeOneLineTextField("<characteristic>");
+		observationCharacteristic.addMouseListener(mListener);
 
-    this.add(WidgetFactory.makeHalfSpacer());
-    this.add(WidgetFactory.makeDefaultSpacer());
+		characteristicPanel.add(observationCharacteristic);
+		characteristicPanel.setBorder(new javax.swing.border.EmptyBorder(0, 0,
+				0, 8 * WizardSettings.PADDING));
 
-  }
+		// Standard
+		JPanel standardPanel = WidgetFactory.makePanel(1);
+		standardPanel.add(WidgetFactory.makeLabel("Standard:", false));
+		observationStandard = WidgetFactory.makeOneLineTextField("<standard>");
+		observationStandard.addMouseListener(mListener);
 
-  private void showDialog(JTextField source) {
+		standardPanel.add(observationStandard);
+		standardPanel.setBorder(new javax.swing.border.EmptyBorder(0, 0, 0,
+				8 * WizardSettings.PADDING));
+
+		// put them together
+		classesPanel.add(entityPanel);
+		classesPanel.add(WidgetFactory.makeDefaultSpacer());
+		classesPanel.add(characteristicPanel);
+		classesPanel.add(WidgetFactory.makeDefaultSpacer());
+		classesPanel.add(standardPanel);
+
+		this.add(classesPanel);
+
+		this.add(WidgetFactory.makeHalfSpacer());
+		this.add(WidgetFactory.makeDefaultSpacer());
+
+	}
+
+	private void showDialog(JTextField source) {
 		OntologyClassSelectionPage page = new OntologyClassSelectionPage();
-		
+
 		// show the dialog
-		ModalDialog dialog = 
-			new ModalDialog(
-					page,
-					UIController.getInstance().getCurrentActiveWindow(),
-					UISettings.POPUPDIALOG_WIDTH,
-					UISettings.POPUPDIALOG_HEIGHT);
-		
-		//get the response back
+		ModalDialog dialog = new ModalDialog(page, UIController.getInstance()
+				.getCurrentActiveWindow(), UISettings.POPUPDIALOG_WIDTH,
+				UISettings.POPUPDIALOG_HEIGHT);
+
+		// get the response back
 		if (dialog.USER_RESPONSE == ModalDialog.OK_OPTION) {
 			source.setText(page.getSelectedTerms().get(0));
 		}
 		page = null;
 	}
-  
-  public void setAnnotation(Annotation a, String attributeName) {
-	  this.annotation = a;
-	  
-	  try {
-		  // what are we editing:
-		  // is there a measurement mapping for the attribute?
-		  currentMapping = annotation.getMapping(attributeName);
-		  currentMeasurement = currentMapping.getMeasurement();
-		  currentCharacteristic = currentMeasurement.getCharacteristics().get(0);
-		  currentStandard = currentMeasurement.getStandard();
 
-		  // is there an observation that uses that measurement?
-		  currentObservation = annotation.getObservation(currentMeasurement);
-		  
-		  //try to set the text field values
-		  try {
-			  String entity = currentObservation.getEntity().getURI();
-			  this.observationEntity.setText(entity);
-		  }
-		  catch (Exception e) {}
-		  try {
-			  String charString = currentCharacteristic.getURI();
-			  this.observationCharacteristic.setText(charString);
-		  }
-		  catch (Exception e) {}
-		  try {
-			  String standard = currentStandard.getURI();
-			  this.observationStandard.setText(standard);
-		  }
-		  catch (Exception e) {}
-	  } catch (Exception e) {
-		  //we don't care about this right now
-	  }
+	public void setAnnotation(Annotation a, String attributeName) {
+		this.annotation = a;
 
-  }
-  
-  public Annotation getAnnotation(String attributeName) {
-	  
-	  // edit the existing values
-	  	if (currentCharacteristic == null) {
+		try {
+			// what are we editing:
+			// is there a measurement mapping for the attribute?
+			currentMapping = annotation.getMapping(attributeName);
+			currentMeasurement = currentMapping.getMeasurement();
+			currentCharacteristic = currentMeasurement.getCharacteristics()
+					.get(0);
+			currentStandard = currentMeasurement.getStandard();
+
+			// is there an observation that uses that measurement?
+			currentObservation = annotation.getObservation(currentMeasurement);
+
+			// try to set the text field values
+			try {
+				String entity = currentObservation.getEntity().getURI();
+				this.observationEntity.setText(entity);
+			} catch (Exception e) {
+			}
+			try {
+				String charString = currentCharacteristic.getURI();
+				this.observationCharacteristic.setText(charString);
+			} catch (Exception e) {
+			}
+			try {
+				String standard = currentStandard.getURI();
+				this.observationStandard.setText(standard);
+			} catch (Exception e) {
+			}
+		} catch (Exception e) {
+			// we don't care about this right now
+		}
+
+	}
+
+	public Annotation getAnnotation(String attributeName) {
+
+		// edit the existing values
+		if (currentCharacteristic == null) {
 			currentCharacteristic = new Characteristic(
 					observationCharacteristic.getText());
 		} else {
@@ -258,8 +263,8 @@ private Standard currentStandard;
 		// }
 		// }
 
-		//the observation
-		//TODO: add to exiting obs?
+		// the observation
+		// TODO: add to exiting obs?
 		if (currentObservation == null) {
 			currentObservation = new Observation();
 			currentObservation.setLabel("observation_"
@@ -267,7 +272,7 @@ private Standard currentStandard;
 			currentObservation.addMeasurement(currentMeasurement);
 			annotation.addObservation(currentObservation);
 		}
-		
+
 		// set up the annotation objects with values from the form
 		Entity entity = new Entity(observationEntity.getText());
 		// set the new values for existing classes
@@ -281,107 +286,113 @@ private Standard currentStandard;
 		return annotation;
 	}
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// *
 
-/**
-   *  The action to be executed when the page is displayed. May be empty
-   */
-  public void onLoadAction() {
-  }
+	/**
+	 * The action to be executed when the page is displayed. May be empty
+	 */
+	public void onLoadAction() {
+	}
 
+	/**
+	 * The action to be executed when the "Prev" button is pressed. May be empty
+	 * 
+	 */
+	public void onRewindAction() {
 
-  /**
-   *  The action to be executed when the "Prev" button is pressed. May be empty
-   *
-   */
-  public void onRewindAction() {
+	}
 
-  }
+	/**
+	 * The action to be executed when the "Next" button (pages 1 to
+	 * last-but-one) or "Finish" button(last page) is pressed. May be empty, but
+	 * if so, must return true
+	 * 
+	 * @return boolean true if wizard should advance, false if not (e.g. if a
+	 *         required field hasn't been filled in)
+	 */
+	public boolean onAdvanceAction() {
 
+		return true;
+	}
 
-  /**
-   *  The action to be executed when the "Next" button (pages 1 to last-but-one)
-   *  or "Finish" button(last page) is pressed. May be empty, but if so, must
-   *  return true
-   *
-   *  @return boolean true if wizard should advance, false if not
-   *          (e.g. if a required field hasn't been filled in)
-   */
-  public boolean onAdvanceAction() { 
-	 
-	  
-	  return true;
-  }
+	/**
+	 * gets the OrderedMap object that contains all the key/value paired
+	 * settings for this particular wizard page
+	 * 
+	 * @return data the OrderedMap object that contains all the key/value paired
+	 *         settings for this particular wizard page
+	 */
+	private OrderedMap returnMap = new OrderedMap();
 
+	public OrderedMap getPageData() {
 
-  /**
-   *  gets the OrderedMap object that contains all the key/value paired
-   *  settings for this particular wizard page
-   *
-   *  @return   data the OrderedMap object that contains all the
-   *            key/value paired settings for this particular wizard page
-   */
-  private OrderedMap returnMap = new OrderedMap();
+		return getPageData(null);
+	}
 
-  public OrderedMap getPageData() {
+	/**
+	 * gets the Map object that contains all the key/value paired settings for
+	 * this particular wizard page
+	 * 
+	 * @param rootXPath
+	 *            the root xpath to prepend to all the xpaths returned by this
+	 *            method
+	 * @return data the Map object that contains all the key/value paired
+	 *         settings for this particular wizard page
+	 */
+	public OrderedMap getPageData(String rootXPath) {
+		return returnMap;
+	}
 
-    return getPageData(null);
-  }
+	/**
+	 * gets the unique ID for this wizard page
+	 * 
+	 * @return the unique ID String for this wizard page
+	 */
+	public String getPageID() {
+		return pageID;
+	}
 
-  /**
-   * gets the Map object that contains all the key/value paired settings for
-   * this particular wizard page
-   *
-   * @param rootXPath the root xpath to prepend to all the xpaths returned by
-   *   this method
-   * @return data the Map object that contains all the key/value paired
-   *   settings for this particular wizard page
-   */
-  public OrderedMap getPageData(String rootXPath) {
-    return returnMap;
-  }
+	/**
+	 * gets the title for this wizard page
+	 * 
+	 * @return the String title for this wizard page
+	 */
+	public String getTitle() {
+		return title;
+	}
 
-  /**
-   *  gets the unique ID for this wizard page
-   *
-   *  @return   the unique ID String for this wizard page
-   */
-  public String getPageID() { return pageID; }
+	/**
+	 * gets the subtitle for this wizard page
+	 * 
+	 * @return the String subtitle for this wizard page
+	 */
+	public String getSubtitle() {
+		return subtitle;
+	}
 
-  /**
-   *  gets the title for this wizard page
-   *
-   *  @return   the String title for this wizard page
-   */
-  public String getTitle() { return title; }
+	/**
+	 * Returns the ID of the page that the user will see next, after the "Next"
+	 * button is pressed. If this is the last page, return value must be null
+	 * 
+	 * @return the String ID of the page that the user will see next, or null if
+	 *         this is te last page
+	 */
+	public String getNextPageID() {
+		return nextPageID;
+	}
 
-  /**
-   *  gets the subtitle for this wizard page
-   *
-   *  @return   the String subtitle for this wizard page
-   */
-  public String getSubtitle() { return subtitle; }
+	/**
+	 * Returns the serial number of the page
+	 * 
+	 * @return the serial number of the page
+	 */
+	public String getPageNumber() {
+		return pageNumber;
+	}
 
-  /**
-   *  Returns the ID of the page that the user will see next, after the "Next"
-   *  button is pressed. If this is the last page, return value must be null
-   *
-   *  @return the String ID of the page that the user will see next, or null if
-   *  this is te last page
-   */
-  public String getNextPageID() { return nextPageID; }
+	public boolean setPageData(OrderedMap data, String _xPathRoot) {
 
-  /**
-   *  Returns the serial number of the page
-   *
-   *  @return the serial number of the page
-   */
-  public String getPageNumber() { return pageNumber; }
-
-
-
-  public boolean setPageData(OrderedMap data, String _xPathRoot) {
-
-    return true;
-  }
+		return true;
+	}
 }
