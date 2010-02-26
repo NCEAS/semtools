f157db1cd36cb15b9f781e198e5ce2c79db32add
Yang Cao <sycao5@gmail.com>
2010-02-26 19:00:01 +0000

    Forget to commit this file.

---
+++ dev/sms/src/org/ecoinformatics/oboe/Constant.java
@@ -0,0 +1,8 @@
+package org.ecoinformatics.oboe;
+
+public class Constant {
+	public static String inputUriPrefix = "https://code.ecoinformatics.org/code/semtools/trunk/dev/sms/examples/";
+	public static String localInputUriPrefix = "/Users/cao/DATA/SONET/svntrunk/semtools/dev/sms/examples/";
+	public static String outputUriPrefix = "https://code.ecoinformatics.org/code/semtools/trunk/dev/oboedb/";
+	public static String localOutputUriPrefix = "/Users/cao/DATA/SONET/svntrunk/semtools/dev/oboedb/";	
+}
