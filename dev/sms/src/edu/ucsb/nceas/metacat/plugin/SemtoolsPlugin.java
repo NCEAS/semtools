5b613e51ae6ae9a9dd4d559d4585d1192b785238
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2010-08-30 23:42:52 +0000

    load ontologies - directly from URIs for now include initialize method - there is room for improvement here

--- dev/sms/src/edu/ucsb/nceas/metacat/plugin/SemtoolsPlugin.java
+++ dev/sms/src/edu/ucsb/nceas/metacat/plugin/SemtoolsPlugin.java
@@ -2,11 +2,11 @@ package edu.ucsb.nceas.metacat.plugin;
 
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Vector;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,24 +17,114 @@ import org.ecoinformatics.sms.annotation.search.Criteria;
 import org.ecoinformatics.sms.annotation.search.CriteriaReader;
 
 import edu.ucsb.nceas.metacat.DBQuery;
+import edu.ucsb.nceas.metacat.DBUtil;
 import edu.ucsb.nceas.metacat.DocumentIdQuery;
+import edu.ucsb.nceas.metacat.client.Metacat;
+import edu.ucsb.nceas.metacat.client.MetacatFactory;
 import edu.ucsb.nceas.metacat.shared.HandlerException;
+import edu.ucsb.nceas.metacat.util.SystemUtil;
 
 public class SemtoolsPlugin implements MetacatHandlerPlugin {
 
 	private static List<String> supportedActions = new ArrayList<String>();
-	
 	static {
+		//note: order matters - it drives the switch/case handling
 		supportedActions.add("semquery");
-		
-		//TODO: load annotations
+		supportedActions.add("initialize");
 	}
 	
+	/**
+	 * Look up annotations stored on this Metacat instance
+	 */
+	private static void initializeAnnotations() {
+		try {
+			String metacatURL = SystemUtil.getServletURL();
+			Metacat mc = MetacatFactory.createMetacatConnection(metacatURL);
+			DBUtil dbutil = new DBUtil();
+			Vector<String> annotationDocids = dbutil.getAllDocidsByType(Annotation.ANNOTATION_NS, false);
+			for (String annotationDocid: annotationDocids) {
+				try {
+					InputStream in = mc.read(annotationDocid);
+					Annotation annotation = Annotation.read(in);
+					SMS.getInstance().getAnnotationManager().importAnnotation(annotation, metacatURL + "/" + annotationDocid);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void initializeOntologies() {
+		// clear the ontologies that are loaded
+		for (String uri:SMS.getInstance().getOntologyManager().getOntologyIds()) {
+			if (SMS.getInstance().getOntologyManager().isOntology(uri)) {
+				SMS.getInstance().getOntologyManager().removeOntology(uri);
+			}
+		}
+		
+		// TODO: load them from BioPortal
+		// load the configured ontologies
+		Hashtable<String, String> ontologyURIs = new Hashtable<String, String>();
+		ontologyURIs.put(
+				"http://ecoinformatics.org/oboe/oboe.1.0/oboe.owl", 
+				"http://ecoinformatics.org/oboe/oboe.1.0/oboe.owl");
+		ontologyURIs.put(
+				"https://code.ecoinformatics.org/code/semtools/trunk/dev/oboe/oboe-sbc.owl", 
+				"https://code.ecoinformatics.org/code/semtools/trunk/dev/oboe/oboe-sbc.owl");
+		ontologyURIs.put(
+				"https://code.ecoinformatics.org/code/sonet/trunk/ontologies/oboe-trait.owl", 
+				"https://code.ecoinformatics.org/code/sonet/trunk/ontologies/oboe-trait.owl");
+		
+		// map them first
+		try {
+			SMS.getInstance().getOntologyManager().mapOntologies(ontologyURIs);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		// load them in the manager
+		for (Entry<String, String> entry: ontologyURIs.entrySet()) {
+			String uri = entry.getKey();
+			String url = entry.getValue();
+			try {
+				SMS.getInstance().getOntologyManager().importOntology(url, uri);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	/**
+	 * delegates to the appropriate handling method
+	 */
 	public boolean handleAction(String action,
 			Hashtable<String, String[]> params, HttpServletRequest request,
 			HttpServletResponse response, String username, String[] groups,
 			String sessionId) throws HandlerException {
 
+		switch (supportedActions.indexOf(action)) {
+		case 0:
+			semquery(params, request, response, username, groups, sessionId);
+			break;
+		case 1:
+			initializeOntologies();
+			initializeAnnotations();
+			break;
+		default:
+			break;
+		}
+		
+		return true;
+	}
+
+	private void semquery(Hashtable<String, String[]> params, HttpServletRequest request,
+			HttpServletResponse response, String username, String[] groups,
+			String sessionId) throws HandlerException {
 		try {
 			// get the semantic query xml
 			String semquery = params.get("query")[0];
@@ -75,7 +165,6 @@ public class SemtoolsPlugin implements MetacatHandlerPlugin {
 			e.printStackTrace();
 			throw new HandlerException(e.getMessage());
 		}
-		return true;
 	}
 
 	public boolean handlesAction(String action) {
